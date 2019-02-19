d47a793e3d237b00ac6e07ee002d0b0e6e3e77a4
Nelson Nyland <nelsonnyland@gmail.com>
2019-02-19 10:22:37 -0800

    First MVC frontend integration after VS Code buildup.

---
+++ NicholasNyland/Scripts/main.js
@@ -0,0 +1,87 @@
+console.log("MAIN CONNECTED");
+"use strict";
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+/**
+ * Adds event listeners to elements on page.
+ */
+function setEventListener() {
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].onclick = accordion;
+    }
+    var slides = document.getElementsByClassName("thumbnail");
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].onclick = slideshow;
+    }
+    var arrows = document.getElementsByClassName("arrow");
+    for (var i = 0; i < arrows.length; i++) {
+        arrows[i].onclick = toggleArrow;
+    }
+}
+
+function accordion() {    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+}
+
+/**
+ * Creates main image depending on current image.
+ * @param {event} e the event created by onclick
+ */
+function slideshow(e) {
+    var div = $("slide");
+    while (div.hasChildNodes()) {
+        div.removeChild(div.firstChild);        
+    }
+    var image = document.createElement("img");
+    image.src = e.target.src;
+    div.appendChild(image);
+}
+
+/**
+ * Toggles the arrow selector on the sidebar.
+ * @param {event} e the event created by onclick
+ */
+function toggleArrow(e) {
+    var arrow = e.target;
+    if (arrow.className == "arrow" ||
+        arrow.className == "arrow right") {
+        arrow.classList.remove("right");
+        arrow.classList.toggle("down");
+        arrow.src = "~/Images/triangledown.png";
+        var ex = arrow.nextElementSibling
+                      .nextElementSibling
+                      .nextElementSibling;
+        ex.style.display = "block";
+    } else {
+        arrow.classList.remove("down");
+        arrow.classList.toggle("right");
+        arrow.src = "~/Images/triangleside.png";
+        var ex = arrow.nextElementSibling
+                      .nextElementSibling
+                      .nextElementSibling;
+        ex.style.display = "none";
+    }
+}
+
+function appendImage() {
+    var div = $("slide");
+    var image = document.createElement("img");
+    var slides = document.getElementsByClassName("thumbnail");
+    image.src = slides[0].src;
+    div.appendChild(image);
+}
+
+window.onload = function() {
+    appendImage();
+    setEventListener();
+}
+
