294a12601d8e7106d3f6628cd633408985911593
Nelson Nyland <nelsonnyland@gmail.com>
2019-02-25 16:36:20 -0800

    Added annotation.

--- NicholasNyland/Models/Objects/Exhibit.cs
+++ NicholasNyland/Models/Objects/Exhibit.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace NicholasNyland.Models
 {
+    [Table("Exhibits")]
     public class Exhibit
     {
         public string Name { get; set; }
