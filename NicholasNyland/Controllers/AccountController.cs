15fb9a9447ab78660731b5c725ee6343b6fbb83f
Nelson Nyland <nelsonnyland@gmail.com>
2019-04-13 12:18:06 -0700

    AccountController bug fixes.

--- NicholasNyland/Controllers/AccountController.cs
+++ NicholasNyland/Controllers/AccountController.cs
@@ -94,20 +94,19 @@ namespace NicholasNyland.Controllers
                 return View(model);
             }
 
-            // This doesn't count login failures towards account lockout
-            // To enable password failures to trigger account lockout, change to shouldLockout: true
-            //var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-
             //if (model.ForgotPassword)
             //{
             //    return RedirectToAction("ForgotPassword", "Account", model.Email);
             //}
 
-            var user = await _userManager.FindByNameAsync(model.Email);
-            if (user != null && (!await _userManager.IsEmailConfirmedAsync(user.Id)))
+            if (model.Email != null)
             {
-                ModelState.AddModelError(string.Empty, "You must have a confirmed email to log in.");
-                return View(model);
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user != null && (!await _userManager.IsEmailConfirmedAsync(user.Id)))
+                {
+                    ModelState.AddModelError(string.Empty, "You must have a confirmed email to log in.");
+                    return View(model);
+                }
             }
 
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: true);
